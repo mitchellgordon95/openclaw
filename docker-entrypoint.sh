d2f18d482744dfd1eed001572631fc8bdad95faf
Mitchell Gordon <mitchell.gordon95@gmail.com>
2026-03-14 17:33:59 -0400

    fix: remove Twilio-specific config from docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,18 +22,8 @@ if [ -n "$OPENCLAW_ORIGIN" ]; then
       cfg.gateway.controlUi.allowedOrigins = ['$OPENCLAW_ORIGIN'];
       cfg.gateway.controlUi.dangerouslyDisableDeviceAuth = true;
       cfg.gateway.trustedProxies = ['100.64.0.0/10', '10.0.0.0/8', '172.16.0.0/12'];
-      if ('$TWILIO_ACCOUNT_SID' && '$TWILIO_AUTH_TOKEN' && '$TWILIO_PHONE_NUMBER') {
-        cfg.channels = cfg.channels || {};
-        cfg.channels['twilio-sms'] = cfg.channels['twilio-sms'] || {};
-        cfg.channels['twilio-sms'].accountSid = '$TWILIO_ACCOUNT_SID';
-        cfg.channels['twilio-sms'].authToken = '$TWILIO_AUTH_TOKEN';
-        cfg.channels['twilio-sms'].phoneNumber = '$TWILIO_PHONE_NUMBER';
-        cfg.channels['twilio-sms'].enabled = true;
-      }
       fs.writeFileSync(file, JSON.stringify(cfg, null, 2));
-      console.log('Seeded controlUi.allowedOrigins =', ['$OPENCLAW_ORIGIN']);
-      console.log('Seeded trustedProxies for Railway internal network');
-      if ('$TWILIO_ACCOUNT_SID') console.log('Seeded twilio-sms channel config');
+      console.log('Seeded gateway config for Railway');
     \"
   "
 fi
